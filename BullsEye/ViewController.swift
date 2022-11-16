dd5450592f1930134a22b9101e927493994fb80e
rigo.dominguezg@gmail.com <rigo.dominguezg@gmail.com>
2022-11-16 12:46:24 -0600

    Changes in ViewController

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,154 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider : UISlider!
+    
+    @IBOutlet var targetLabel : UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var roundlabel: UILabel!
+    
+    var currentValue = 0
+    
+    var targetValue = 0
+    
+    var score = 0
+    
+    var round = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        /*targetValue = Int.random(in: 1...100)
+        currentValue = lroundf(slider.value)*/
+        startNewGame()
+        customSlider()
+        
     }
-
-
+    
+    
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundlabel.text = String(round
+        )
+    }
+   
+    
+    @IBAction func showAlert() {
+        
+       /* var difference : Int
+        
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }*/
+        
+        /*var difference = currentValue - targetValue
+        
+        if difference < 0 {
+            difference *= -1
+        }*/
+        
+        let difference = abs(currentValue - targetValue)
+        
+        var points = 100 - difference
+        
+        /*let message = "The value of the slider is now: \(currentValue)" +
+        "\nThe target value is: \(targetValue)" +
+        "\nThe difference is: \(difference) "*/
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(
+            title : title,
+            message : message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: {
+                _ in self.startNewRound()
+            })
+        
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        //print ("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
+    }
+    
+    func customSlider(){
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(
+            named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(
+        top: 0,
+        left: 14,
+        bottom: 0,
+        right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(
+            withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(
+            withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+    }
 }
 

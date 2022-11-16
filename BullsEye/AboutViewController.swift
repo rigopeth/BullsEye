f4023c52f37f63a471f31987f50867fd015a90f3
rigo.dominguezg@gmail.com <rigo.dominguezg@gmail.com>
2022-11-16 12:46:01 -0600

    Add AboutViewController

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet var webView: WKWebView!
+    
+    @IBAction func close() {
+        dismiss(animated: true,completion: nil)
+    }
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      if let url = Bundle.main.url(
+        forResource: "BullsEye", withExtension: "html") {
+        let request = URLRequest(url: url)
+        webView.load(request)
+      }
+    }
+
+
+}
